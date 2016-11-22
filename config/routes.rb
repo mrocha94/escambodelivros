199f464873ced4957ae050d2a8c3a15415aea335
Mateus Rocha <mateusmazzari@usp.br>
2016-11-22 17:52:36 -0200

    sistema de autenticação

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,26 @@
 Rails.application.routes.draw do
+  resources :passwords,
+            controller: 'clearance/passwords',
+            only: [:create, :new]
+
+  resource :session,
+           controller: 'clearance/sessions',
+           only: [:create]
+
+  resources :users,
+            controller: 'clearance/users',
+            only: Clearance.configuration.user_actions do
+    resource :password,
+             controller: 'clearance/passwords',
+             only: [:create, :edit, :update]
+  end
+
+  get '/sign_in' => 'clearance/sessions#new', as: 'sign_in'
+  delete '/sign_out' => 'clearance/sessions#destroy', as: 'sign_out'
+
+  if Clearance.configuration.allow_sign_up?
+    get '/sign_up' => 'clearance/users#new', as: 'sign_up'
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
