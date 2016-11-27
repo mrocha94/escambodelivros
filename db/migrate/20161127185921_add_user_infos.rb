1de0e9fb235abcdfa73158e53c3add9fe6defa32
Mateus Rocha <mateusmazzari@usp.br>
2016-11-27 18:11:40 -0200

    user with all params

---
+++ db/migrate/20161127185921_add_user_infos.rb
@@ -0,0 +1,10 @@
+class AddUserInfos < ActiveRecord::Migration
+  def change
+    change_table :users do |t|
+      t.string :nome, null: false
+      t.string :cpf, limit: 11, null: false
+      t.date :data_nascimento, null: false
+      t.string :telefone, limit: 11, null: false
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161122193736) do
+ActiveRecord::Schema.define(version: 20161127185921) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
     t.datetime "created_at",                     null: false
@@ -20,9 +23,13 @@ ActiveRecord::Schema.define(version: 20161122193736) do
     t.string   "encrypted_password", limit: 128, null: false
     t.string   "confirmation_token", limit: 128
     t.string   "remember_token",     limit: 128, null: false
+    t.string   "nome",                           null: false
+    t.string   "cpf",                limit: 11,  null: false
+    t.date     "data_nascimento",                null: false
+    t.string   "telefone",           limit: 11,  null: false
   end
 
-  add_index "users", ["email"], name: "index_users_on_email"
-  add_index "users", ["remember_token"], name: "index_users_on_remember_token"
+  add_index "users", ["email"], name: "index_users_on_email", using: :btree
+  add_index "users", ["remember_token"], name: "index_users_on_remember_token", using: :btree
 
 end
