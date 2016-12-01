a4c514aa71aff0682ebde9ee4eb48b165d515db1
Mateus Rocha <mateusmazzari@usp.br>
2016-12-01 01:00:28 -0200

    layout

--- config/routes.rb
+++ config/routes.rb
@@ -22,7 +22,7 @@ Rails.application.routes.draw do
 
   get '/sign_in' => 'clearance/sessions#new', as: 'sign_in'
   delete '/sign_out' => 'clearance/sessions#destroy', as: 'sign_out'
-
+  root to: 'advertisements#index'
   if Clearance.configuration.allow_sign_up?
     get '/sign_up' => 'users#new', as: 'sign_up'
   end
